99f3fcfd3d3720a27e132e822a3fe7791a88e8be
Dmitry <divgorbunov@gmail.com>
2021-07-21 23:24:58 +0300

    Add randomization to rotation.

--- Assets/DiceScript.cs
+++ Assets/DiceScript.cs
@@ -4,6 +4,7 @@ public class DiceScript : MonoBehaviour
 {
     private static Rigidbody rb;
     public static Vector3 diceVelocity;
+    public float force = 500;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +17,17 @@ public class DiceScript : MonoBehaviour
     {
         diceVelocity = rb.velocity;
 
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetTouch(0).phase==TouchPhase.Began)
+        if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             float dirX = Random.Range(0, 500);
             float dirY = Random.Range(0, 500);
             float dirZ = Random.Range(0, 500);
+            float rotX = Random.Range(0, 500);
+            float rotY = Random.Range(0, 500);
+            float rotZ = Random.Range(0, 500);
             transform.position = new Vector3(0, 2, 0);
-            transform.rotation = Quaternion.identity;
-            rb.AddForce(transform.up * 500);
+            transform.rotation = Quaternion.Euler(new Vector3(rotX, rotY,rotZ));
+            rb.AddForce(Vector3.up * force);
             rb.AddTorque(dirX, dirY, dirZ);
         }
     }
